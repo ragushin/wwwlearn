8f83a0d8c42b8e57c806facf72fabdd353086069
ragushin <ragushin@gmail.com>
2016-11-10 10:49:51 +0200

    3

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-ln -s /home/box/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo rm -r /etc/nginx/sites-enabled/default
+ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
