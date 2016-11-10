fdbe388686f9f43307656402e3337bf87fc457c0
ragushin <ragushin@gmail.com>
2016-11-10 15:58:57 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+def wsgi_app(env,start_response):
+    status = '200 OK'
+    headers=[('Content-Type', 'text/plain')]
+    start_response(status,headers)
+    out = env['QUERY_STRING'].split("&")
+    out =[item+'\r\n' for item in out]
+    return out
