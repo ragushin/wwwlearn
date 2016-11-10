09742a785b91b5c7fece23f8acbf5762defd4bf9
ragushin <ragushin@gmail.com>
2016-11-10 17:13:52 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo rm -r /etc/nginx/sites-enabled/default
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
+sudo gunicorn - b 0.0.0.0:8080 hello
 
 
 
