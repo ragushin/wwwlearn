fc9a29d17b832cf55f50f717cd5a92872f6f3f25
ragushin <ragushin@gmail.com>
2016-11-11 15:05:59 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@ sudo rm -r /etc/nginx/sites-enabled/default
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo gunicorn -b 0.0.0.0:8080 hello &
-
+sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
 
 
