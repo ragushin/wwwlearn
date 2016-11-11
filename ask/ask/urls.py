227de6b52ff28afc36f15540cb1d05ae86e08191
ragushin <ragushin@gmail.com>
2016-11-11 21:44:56 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from django.conf.urls import url                                                
 from django.contrib import admin
+from qa.views import test
 
 admin.autodiscover()
 
