9035b38c679ea20dd19a20f75cc1c54bd42b356e
ragushin <ragushin@gmail.com>
2016-11-11 14:07:11 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ def application(env,start_response):
     status = '200 OK'
     headers=[('Content-Type', 'text/plain')]
     start_response(status,headers)
-    out = env['QUERY_STRING'].split("&")
-    out =[item+'\r\n' for item in out]
-    return out
+    resp = env['QUERY_STRING'].split("&")
+    resp = [item+"\r\n" for item in resp]
+    return resp
