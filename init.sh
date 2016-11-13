a8e7922f26dca9a2b0b627424593f1ecb2d26480
ragushin <ragushin@gmail.com>
2016-11-13 12:16:33 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,12 @@
 sudo rm -r /etc/nginx/sites-enabled/default
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -b 0.0.0.0:8080 hello &
-sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
+#sudo gunicorn -b 0.0.0.0:8080 hello &
+#sudo gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
+
+virtualenv --python=/usr/bin/python3.4 web/myvenv 
+web/myvenv source web/myvenv/bin/activate
+sudo gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello:app &     
+sudo gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application & 
 
 
