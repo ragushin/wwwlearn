512236aa41554bdfefb2059f00c834d5c0cac7bd
ragushin <ragushin@gmail.com>
2016-11-13 16:03:31 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p!yco*_(sm4kg#rt3qzn=90pm!uapdr4n6*v)io71^*ww8%eg0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
