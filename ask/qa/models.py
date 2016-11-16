61b4a660eb6dff76311d2995e511dfc38e6130ba
ragushin <ragushin@gmail.com>
2016-11-16 11:14:09 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+Class Question(models.Model):
+  objects = QuestionManager() 
+  
+  title=models.CharField()
+  text=models.TextField()
+  added_at=models.DateField()
+  rating=models.IntegerField()
+  author=models.CharField()
+  likes=models.TextField()
+
+  
+Class Answer(models.Model):
+  text=models.TextField()
+  added_at=models.DateField()
+  question=models.CharField()
+  author=models.CharField()
+  
+  
+class QuestionManager(models.Manager):                                          
+  def new():                                                              
+    pass                                                            
+  def popular():                                                          
+    pass 
